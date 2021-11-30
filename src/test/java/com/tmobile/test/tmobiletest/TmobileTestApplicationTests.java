a6addfd44e8d6aa11814d208d5b81d2069b107b5
porozcog <porozco87@gmail.com>
2021-11-30 12:51:05 -0700

    Add files via upload

---
+++ src/test/java/com/tmobile/test/tmobiletest/TmobileTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmobile.test.tmobiletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmobileTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
